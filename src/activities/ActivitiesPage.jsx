c58c8a7b8197e090635576a31eed8b1a79dc1186
Hailey Burgess <hailey.burgess@redcross.org>
2025-05-28 21:44:04 -0400

    can see delete button when logged in

--- src/activities/ActivitiesPage.jsx
+++ src/activities/ActivitiesPage.jsx
@@ -1,6 +1,7 @@
 import { act } from "react";
 import useMutation from "../api/useMutation";
 import useQuery from "../api/useQuery";
+import { useAuth } from "../auth/AuthContext";
 
 export default function ActivitiesPage() {
   //fetch activities from the API and provide a tag
@@ -10,6 +11,8 @@ export default function ActivitiesPage() {
     loading,
     error,
   } = useQuery("/activities", "activities");
+
+  const { token } = useAuth();
 
   //mutation to POST to /activities and invalidate the "activities" tag to force a refetch from the API
   //renaming variables as in demo so as to not conflict w/ query variables
@@ -26,7 +29,7 @@ export default function ActivitiesPage() {
     const description = formData.get("description");
     mutate({ name, description });
   };
-console.log(activities);
+  console.log(activities);
   return (
     <>
       <h1>Activities</h1>
@@ -34,7 +37,10 @@ console.log(activities);
       {error && <p>Error loading activities: {error.message}</p>}
       {activities?.length > 0 ? (
         activities.map((activity) => (
-          <p key={activity.id}>{activity.name}</p>
+          <p key={activity.id}>
+            {activity.name}
+            {token ? <button>Delete</button> : null}
+          </p>
         ))
       ) : (
         <p>No activities found.</p>
